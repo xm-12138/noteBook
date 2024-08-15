b2f8823eb670cd06b275735ca4595ac288668ac4
兮梦 <xm121381@163.com>
2024-08-15 13:57:25 +0800

    增加帕鲁服务器

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,6 +13,15 @@ export default defineUserConfig({
             link: '/'
         },
         {
+            text: '前端',
+            children:[
+                {
+                    text: 'js',
+                    link: '/前端/js.html',
+                }
+            ]
+        },
+        {
             text: '大数据',
             children:[
                 {
@@ -27,15 +36,6 @@ export default defineUserConfig({
                     text:'数据分析',
                     link: '/大数据/数据分析.html',
                 },
-            ]
-        },
-        {
-            text: '前端',
-            children:[
-                {
-                    text: 'js',
-                    link: '/前端/js.html',
-                }
             ]
         },
         {
@@ -59,7 +59,17 @@ export default defineUserConfig({
                 }
                 
             ]
+        },
+        {
+            text: '游戏',
+            children:[
+                {
+                    text:'帕鲁',
+                    link: '/游戏/帕鲁.html',
+                },
+            ]
         }
+
     ]
   }),
 })
